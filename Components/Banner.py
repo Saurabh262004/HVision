349567c098c1b4276c3b31ad24fa10ec8c87d1a1
Saurabh262004 <saurabhrjadhav26@gmail.com>
2025-11-18 20:52:41 +0530

    let's not...

--- Components/Banner.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-class Banner:
-  def __init__(self, title: str, standardItems: List[str], softPityStart: int = None, hardPity: int = None, rateUpItems: List[str] = []):
-    self.title = title
-    self.softPityStart = softPityStart
-    self.hardPity = hardPity
-    self.standardItems = standardItems
-    self.rateUpItems = rateUpItems
--- Components/__init__.py
+++
@@ -1,4 +0,0 @@
-from Components.Account import Account
-from Components.Banner import Banner
-from Components.Flow import Flow
-from Components.Game import Game
