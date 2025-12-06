98bd53e38ea288a957088cd2ca9cd9356f364392
Saurabh262004 <saurabhrjadhav26@gmail.com>
2025-12-06 13:05:41 +0530

    holy typos

--- CMD/RuntimeOptions.py
+++ CMD/RuntimeOptions.py
@@ -6,14 +6,14 @@ def help():
   print('\n ----- HVision ----- \n')
 
   for k, v in optionsDict.items():
-    print(f'{k:<25}: {v['disc']}')
+    print(f'{k:<25}: {v['desc']}')
 
   print('')
 
 def setupRuntimeOptions():
-  optionsDict['help'] = {'fn': help, 'disc': 'Print this output.'}
-  optionsDict['updateDB'] = {'fn': DBProtocols.generateDB, 'disc': 'Force update the database.'}
-  optionsDict['verifyDB'] = {'fn': DBProtocols.verifyDB, 'disc': 'Verfies the database.'}
-  optionsDict['verifyDBBackground'] = {'fn': DBProtocols.verifyDBBackground, 'disc': 'Verifies the database in a separate thread.'}
+  optionsDict['help'] = {'fn': help, 'desc': 'Print this output.'}
+  optionsDict['updateDB'] = {'fn': DBProtocols.generateDB, 'desc': 'Force update the database.'}
+  optionsDict['verifyDB'] = {'fn': DBProtocols.verifyDB, 'desc': 'Verifies the database.'}
+  optionsDict['verifyDBBackground'] = {'fn': DBProtocols.verifyDBBackground, 'desc': 'Verifies the database in a separate thread.'}
 
 setupRuntimeOptions()
